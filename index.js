8bfaf5b8f361a71467cec5be9f7c34d8fcc12fed
Kamary <kamarygillespie@Kamarys-MBP.lan>
2022-08-26 08:47:02 -0600

    created questions that will generate the readme

--- index.js
+++ index.js
@@ -1,15 +1,59 @@
 // TODO: Include packages needed for this application
 const fs = require('fs');
 const inquirer = require("inquirer");
+const generateMarkdown = require('./utils/generateMarkdown');
 
 // TODO: Create an array of questions for user input
-const questions = [];
+const questions = [{
+        type: "input",
+        message: "What is the title of your project?",
+        name: "title",
+    }, {
+        type: "input",
+        message: "Please provide a description of your project",
+        name: "project-description",
+    }, {
+        type: "input",
+        message: "How do you install the application?",
+        name: "installation",
+    }, {
+        type: "input",
+        message: "How is this application used?",
+        name: "usage",
+    }, {
+        type: "input",
+        message: "Contribution credit?",
+        name: "contribution",
+    }, {
+        type: "input",
+        message: "test instrucions?",
+        name: "test-instructions"
+    }, {
+        type: "list",
+        message: "Please choose a license for this project",
+        choices: ["Apache License 2.0", "GNU GPLv3", "MIT", ],
+        name: "license-selection"
+    }
 
-// TODO: Create a function to write README file
-function writeToFile(fileName, data) {}
+
+
+
+
+
+
+
+];
+
+//TODO: Create a function to write README file
+function writeToFile(fileName, data) {
+
+}
+
 
 // TODO: Create a function to initialize app
-function init() {}
+function init() {
+
+}
 
 // Function call to initialize app
 init();
